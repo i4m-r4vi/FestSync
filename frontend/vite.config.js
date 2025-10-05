c587be2b4966a2c9dea869e88b1cc3d955e0160a
i4m-r4vi <ayswarym04@gmail.com>
2025-10-06 01:33:38 +0530

    Size Reduced frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          motion: ['framer-motion'],
+          query: ['@tanstack/react-query'],
+          stripe: ['@stripe/stripe-js'],
+          utils: ['axios', 'date-fns'],
+          icons: ['lucide-react'],
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       "/api": {
